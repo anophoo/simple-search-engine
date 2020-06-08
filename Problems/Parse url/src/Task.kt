cbcf2965a927a18ddbd4acc25a0d64c41b3312e2
root <ana.gurgenidze11@gmail.com>
2020-06-08 12:26:58 +0400

    add parse url solution

--- Problems/Parse url/src/Task.kt
+++ Problems/Parse url/src/Task.kt
@@ -1,31 +1,18 @@
-import java.util.*
-
 fun main() {
-    val scanner = Scanner(System.`in`)
-    val url = scanner.next() + "&"
-    val pass = if (url.indexOf("pass=") != -1) url.substringAfter("pass=").substringBefore("&") else ""
-    val port = if (url.indexOf("port=") != -1) url.substringAfter("port=").substringBefore("&") else ""
-    val cookie = if (url.indexOf("cookie=") != -1) url.substringAfter("cookie=").substringBefore("&") else ""
-    val host = if (url.indexOf("host=") != -1) url.substringAfter("host=").substringBefore("&") else ""
-    if (pass.isNotEmpty()) {
-        println("pass : $pass")
+    var password: String? = null
+    val searchParams = readLine()!!.substringAfter("?").split("&")
+        .map { it.split("=", limit = 2) }
+        .toMutableList()
+
+    searchParams.forEach { (key, value) ->
+        if (key == "pass" && value.isNotEmpty()) {
+            password = value
+        }
+
+        println("$key : ${if (value.isNotEmpty()) value else "not found"}")
     }
-    if (port.isNotEmpty()) {
-        println("port : $port")
-    } else {
-        println("port : not found")
-    }
-    if (cookie.isNotEmpty()) {
-        println("cookie : $cookie")
-    } else {
-        println("cookie : not found")
-    }
-    if (host.isNotEmpty()) {
-        println("host : $host")
-    } else {
-        println("host : not found")
-    }
-    if (pass.isNotEmpty()) {
-        println("password : $pass")
+
+    if (password != null) {
+        println("password : $password")
     }
 }
